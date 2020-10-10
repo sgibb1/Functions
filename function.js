f7bcf7b635789523a61a0c04af7aba244e213be7
Shaun Gibb <sgibb1@mail.valenciacollege.edu>
2020-10-10 13:40:05 -0400

    Added functions divisibleByTwo and largestNum

---
+++ function.js
@@ -0,0 +1,26 @@
+//First Name
+
+
+//divisibleByTwo
+function divisibleByTwo(num) {
+  if (num % 2 == 0)
+    return true;
+  else
+    return false;
+}
+
+console.log(divisibleByTwo(5))
+console.log(divisibleByTwo(8))
+console.log(divisibleByTwo(33))
+console.log(divisibleByTwo(68))
+console.log(divisibleByTwo(95))
+
+//largetsNum
+function largestNum(input) {
+  if (toString.call(input) !== "[object Array]")
+    return false;
+  return Math.max.apply(null, input);
+}
+
+console.log(largestNum([0, 15, 99, 1001, 45]));
+console.log(largestNum([-3, -55, -1, -3, 8]));
